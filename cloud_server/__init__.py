53cef1cfd2d2011240bcaaabe831a6ea499dc9d2
Vladkolo <vladkolo2014@gmail.com>
2023-02-01 16:32:45 +0200

    Update __init__.py
    added config

--- cloud_server/__init__.py
+++ cloud_server/__init__.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from cloud_server import Users
-# from cloud_server import AWSCloud 
-import cryptocode as crypto
-import uuid
-import hashlib
-from markupsafe import escape
 
 
 PROJECT_FOLDER = "cloud_server"
 
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///keyforaws.db"
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
+
 db = SQLAlchemy(app)
 
 from cloud_server import routes
