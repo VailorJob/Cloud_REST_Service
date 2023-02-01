d718db847befb146b6e4dc001f5ce53bc0257e0b
Vladkolo <vladkolo2014@gmail.com>
2023-02-01 16:32:55 +0200

    Update aws_cloud.py

--- cloud_server/aws_cloud.py
+++ cloud_server/aws_cloud.py
@@ -1,25 +1,27 @@
 import boto3
+from typing import List
+
 
 class AWSCloud:
-    s3_resource = boto3.resource('s3')
-    bucket_for_files = s3_resource.Bucket("files-from-the-test-rest-api")
-    if not "files-from-the-test-rest-api" in [i.name for i in s3_resource.buckets.all()]:
-        bucket_for_files.create()
-
-    @classmethod    
-    def get_files_keys(cls) -> list[str]:
-        files = [obj for obj in cls.bucket_for_files.objects.all()]
+    def __init__():
+        self.s3_resource = boto3.resource('s3')
+        self.bucket_for_files = self.s3_resource.Bucket("files-from-the-test-rest-api")
+        if not "files-from-the-test-rest-api" in [i.name for i in self.s3_resource.buckets.all()]:
+            self.bucket_for_files.create()
+ 
+    def get_files_keys(self) -> List[str]:
+        files = [obj for obj in self.bucket_for_files.objects.all()]
         return files
 
     @classmethod
-    def get_file(cls, _key: str) -> bytes:
-        return cls.bucket_for_files.Object(_key).get()["Body"].read()
+    def get_file(self, _key: str) -> bytes:
+        return self.bucket_for_files.Object(_key).get()["Body"].read()
 
     @classmethod
-    def put_file(cls, _key: str, body_file: bytes) -> None:
-        cls.bucket_for_files.put_object(Key=_key, Body=body_file)
+    def put_file(self, _key: str, body_file: bytes) -> None:
+        self.bucket_for_files.put_object(Key=_key, Body=body_file)
 
     @classmethod
-    def delete_file(cls, _key: str) -> None:
-        cls.bucket_for_files.Object(_key).delete()
+    def delete_file(self, _key: str) -> None:
+        self.bucket_for_files.Object(_key).delete()
 
