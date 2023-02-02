46fda8e9ae1ae1812ced118aca7e097cfd9d3242
Vladkolo <40445579+VailorJob@users.noreply.github.com>
2023-02-02 03:12:27 +0200

    Update __init__.py
    Add check file config

--- cloud_server/__init__.py
+++ cloud_server/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import path
 
 PROJECT_FOLDER = "cloud_server"
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+if path.exists("instance/config.py"):
+    app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
 from cloud_server import routes
-
