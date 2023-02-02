ea1fcc0ec4391432eaba116ea5c16fe40ed3a441
Vladkolo <vladkolo2014@gmail.com>
2023-02-02 15:57:15 +0200

    Update __init__.py

--- cloud_server/__init__.py
+++ cloud_server/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 
-PROJECT_FOLDER = "cloud_server"
-
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config')
